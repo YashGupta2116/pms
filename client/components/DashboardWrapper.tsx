7a0107fe63f6308132bbe6d417d0246e8b508d28
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-06-30 18:07:59 +0530

    Intitial Dashboard Setup completed along with basic Redux toolkit and Navbar, Sidebar

---
+++ client/components/DashboardWrapper.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import StoreProvider, { useAppSelector } from "@/app/redux";
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* Sidebar */}
+      <Sidebar />
+      <main
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${isSidebarCollapsed ? "" : "md:pl-64"}`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
+
+export default DashboardWrapper;
