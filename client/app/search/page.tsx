dde2338bf1e5171c399bb2c121ba2e9bebedcac3
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-07-06 07:18:05 +0530

    Add Search component with debounced input and results display

---
+++ client/app/search/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import ProjectCard from "@/components/Cards/ProjectCard";
+import UserCard from "@/components/Cards/UserCard";
+import Header from "@/components/Header";
+import TaskCard from "@/components/TaskCard";
+import { useSearchQuery } from "@/state/api";
+import { debounce } from "lodash";
+import React, { useEffect, useState } from "react";
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const {
+    data: searchResults,
+    isLoading,
+    isError,
+  } = useSearchQuery(searchTerm, {
+    skip: searchTerm.length < 3,
+  });
+
+  const handleSearch = debounce(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchTerm(event.target.value);
+    },
+    500,
+  );
+
+  useEffect(() => {
+    return handleSearch.cancel;
+  }, [handleSearch.cancel]);
+
+  return (
+    <div className="p-8">
+      <Header name="Search" />
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-1/2 rounded border p-3 shadow dark:border-white dark:text-white"
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="p-5">
+        {isLoading && <p>Laoding...</p>}
+        {isError && (
+          <p className="text-red-500">
+            Error Occured while fetching search results.
+          </p>
+        )}
+        {!isLoading && !isError && searchResults && (
+          <div>
+            {searchResults.tasks && searchResults.tasks?.length > 0 && (
+              <h2>Tasks</h2>
+            )}{" "}
+            {searchResults.tasks?.map((task) => (
+              <TaskCard key={task.id} task={task} />
+            ))}
+            {searchResults.projects && searchResults.projects?.length > 0 && (
+              <h2>Projects</h2>
+            )}{" "}
+            {searchResults.projects?.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+            {searchResults.users && searchResults.users?.length > 0 && (
+              <h2>Users</h2>
+            )}{" "}
+            {searchResults.users?.map((user) => (
+              <UserCard key={user.userId} user={user} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
